9dbbbe0db4678e690cdff3455c3108ecb978d5cd
barbosaoli <gustavobarbosa7162@gmail.com>
2022-01-23 16:06:24 -0300

    create routes

--- Back/index.js
+++ Back/index.js
@@ -1,7 +1,33 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get("");
+let user;
+let tweets = [];
+
+app.post("/sing-up", (req, res) => {
+  user = req.body;
+
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  tweets.push(req.body);
+
+  res.send("OK");
+});
+
+app.get("/tweets", (req, res) => {
+  const auxArr = [];
+
+  for (let i = tweets.length - 1; i >= tweets.length - 10; i--) {
+    if (i < 0) break;
+
+    auxArr.push({ ...user });
+  }
+});
 
 app.listen(5000);
