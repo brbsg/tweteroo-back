6e6f7027100dd113aceae53e1da8e7298cffce0a
barbosaoli <gustavobarbosa7162@gmail.com>
2022-01-24 15:32:31 -0300

    conclusion first part

--- Back/index.js
+++ Back/index.js
@@ -8,26 +8,39 @@ app.use(express.json());
 let user;
 let tweets = [];
 
-app.post("/sing-up", (req, res) => {
+//=========================================================================
+
+app.post("/sign-up", (req, res) => {
+  console.log(req.body);
   user = req.body;
 
   res.send("OK");
 });
 
+//=========================================================================
+
 app.post("/tweets", (req, res) => {
+  console.log(req.body);
   tweets.push(req.body);
 
   res.send("OK");
 });
 
+//=========================================================================
+
 app.get("/tweets", (req, res) => {
+  console.log(req.body);
   const auxArr = [];
 
   for (let i = tweets.length - 1; i >= tweets.length - 10; i--) {
     if (i < 0) break;
 
-    auxArr.push({ ...user });
+    auxArr.push({ ...user, tweet: tweets[i].tweet });
   }
+
+  res.send(auxArr);
 });
+
+//=========================================================================
 
 app.listen(5000);
